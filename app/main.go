49614345b78bea5c90ed1dd5b771b4b3c5365d6e
venkat1701 <thejeastdev@gmail.com>
2026-03-25 16:47:29 +0530

    codecrafters submit [skip ci]

--- app/main.go
+++ app/main.go
@@ -12,39 +12,42 @@ func main() {
 	// You can use print statements as follows for debugging, they'll be visible when running tests.
 	fmt.Println("Logs from your program will appear here!")
 
-	// TODO: Uncomment the code below to pass the first stage
-	//
-	// udpAddr, err := net.ResolveUDPAddr("udp", "127.0.0.1:2053")
-	// if err != nil {
-	// 	fmt.Println("Failed to resolve UDP address:", err)
-	// 	return
-	// }
-	//
-	// udpConn, err := net.ListenUDP("udp", udpAddr)
-	// if err != nil {
-	// 	fmt.Println("Failed to bind to address:", err)
-	// 	return
-	// }
-	// defer udpConn.Close()
-	//
-	// buf := make([]byte, 512)
-	//
-	// for {
-	// 	size, source, err := udpConn.ReadFromUDP(buf)
-	// 	if err != nil {
-	// 		fmt.Println("Error receiving data:", err)
-	// 		break
-	// 	}
-	//
-	// 	receivedData := string(buf[:size])
-	// 	fmt.Printf("Received %d bytes from %s: %s\n", size, source, receivedData)
-	//
-	// 	// Create an empty response
-	// 	response := []byte{}
-	//
-	// 	_, err = udpConn.WriteToUDP(response, source)
-	// 	if err != nil {
-	// 		fmt.Println("Failed to send response:", err)
-	// 	}
-	// }
+	/* ResolveUDPAddr returns an address of UDP end point.
+	The network must be a UDP network name.
+	If the host in the address parameter is not a literal IP address or the port is not a literal port number, ResolveUDPAddr resolves the address to an address of UDP end point. Otherwise, it parses the address as a pair of literal IP address and port number. The address parameter can use a host name, but this is not recommended, because it will return at most one of the host name's IP addresses. */
+
+	udpAddr, err := net.ResolveUDPAddr("udp", "127.0.0.1:2053")
+	if err != nil {
+		fmt.Println("Failed to resolve UDP address:", err)
+		return
+	}
+
+	/*
+		ListenUDP acts like ListenPacket for UDP networks. The network must be a UDP network name; see func Dial for details.
+		If the IP field of laddr is nil or an unspecified IP address, ListenUDP listens on all available IP addresses of the local system except multicast IP addresses. If the Port field of laddr is 0, a port number is automatically chosen.
+	*/
+	udpConn, err := net.ListenUDP("udp", udpAddr)
+	if err != nil {
+		fmt.Println("Failed to bind to address:", err)
+		return
+	}
+	defer udpConn.Close()
+	buf := make([]byte, 512)
+
+	for {
+		size, source, err := udpConn.ReadFromUDP(buf)
+		if err != nil {
+			fmt.Println("Error receiving data:", err)
+			break
+		}
+		receivedData := string(buf[:size])
+		fmt.Printf("Received %d bytes from %s: %s\n", size, source, receivedData)
+
+		// Create an empty response
+		response := []byte{}
+		_, err = udpConn.WriteToUDP(response, source)
+		if err != nil {
+			fmt.Println("Failed to send response:", err)
+		}
+	}
 }
